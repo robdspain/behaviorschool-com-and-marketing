6c64ab3c777db1ab283901e890c57f492af32122
Rob Spain <rob@robspain.com>
2026-03-04 16:08:40 -0800

    Remove dropdown menus from nav

--- src/components/header/config.ts
+++ src/components/header/config.ts
@@ -1,28 +1,10 @@
 import type { MenuSection } from "./types";
 
 export const menuSections: MenuSection[] = [
-  {
-    label: "Tools",
-    children: [
-      { label: "All Tools", href: "/products" },
-      { label: "BehaviorSchool Pro", href: "/pro" },
-      { label: "FBA-to-BIP Generator", href: "/fba-to-bip" },
-      { label: "IEP Goal Generator", href: "/iep-goal-generator" },
-
-    ],
-  },
-  {
-    label: "Study",
-    children: [
-      { label: "BCBA Exam Prep", href: "/study" },
-      { label: "Free Practice Exam", href: "/free-bcba-practice" },
-      { label: "Free Study Plan", href: "/free-study-plan" },
-      { label: "BCBA Study Tools", href: "/bcba-study-tools" },
-    ],
-  },
+  { label: "Products", href: "/products" },
+  { label: "Study", href: "https://study.behaviorschool.com" },
   { label: "Supervise", href: "/supervisors" },
   { label: "Transformation", href: "/transformation-program" },
-  { label: "Community", href: "/community" },
   { label: "Blog", href: "/blog" },
   { label: "About", href: "/about" },
 ];
